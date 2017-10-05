8fd2abfa52d63d98d234247181a8e8e2278246dc
dbicanic <dbicanic77@gmail.com>
2017-10-04 21:55:55 -0500

    Have a working solution, with some online help

--- smallestCommons.js
+++ smallestCommons.js
@@ -1,34 +1,23 @@
-
 function smallestCommons(arr) {
-  arr.sort();
-  for( var i = arr[0]+1; i < arr[1]; i++){
-    arr.push(i);
-  }
-  
-  arr.sort();
-  
-  if( arr[0] == 1){
-    arr.shift();
-  }
-  
-  var lastElm = arr[arr.length - 1];
-  var multiple = lastElm;
-  console.log(arr);
-  
-  for( var j = 0; j < arr[lastElm]; j++ ){
-    while(true){
-      if( multiple % arr[j] == 0 ){
-        break;
-      } else {
-        multiple = multiple + lastElm;
-      }
+    var range = [];
+    for (var i = Math.max(arr[0], arr[1]); i >= Math.min(arr[0], arr[1]); i--) {
+    range.push(i);
     }
-  }
 
-  
-  
-  return multiple;
+    var lcm = range[0];
+    for (i = 1; i < range.length; i++) {
+    var GCD = gcd(lcm, range[i]);
+    lcm = (lcm * range[i]) / GCD;
+    }
+    return lcm;
+
+    function gcd(x, y) { 
+    if (y === 0)
+        return x;
+    else
+        return gcd(y, x%y);
+    }
 }
 
-
+// test here
 smallestCommons([1,5]);
