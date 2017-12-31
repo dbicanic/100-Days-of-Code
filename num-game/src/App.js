e92dd121e465f4ab83cd30ae064521deff80d111
dbicanic <dbicanic77@gmail.com>
2017-12-30 20:35:06 -0600

    Get lodash doing and move on to step 6

--- num-game/src/App.js
+++ num-game/src/App.js
@@ -1,50 +1,104 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import lodash;
+import _ from 'lodash';
 import logo from './logo.svg';
 import './App.css';
 
-// TARGET SUM - Starting Template
+const colors = {
+  new: 'lightblue',
+  playing: 'deepskyblue',
+  won: 'lightgreen',
+  lost: 'lightcoral',
+};
+
 const randomNumberBetween = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
 class Number extends React.Component {
   render() {
-    return <div className="number">{this.props.value}</div>;
+    return (
+      <div
+        className="number"
+        style={{ opacity: this.props.clickable ? 1 : 0.3 }}
+        onClick={() => console.log(this.props.id)}
+      >
+        {this.props.value}
+      </div>
+    );
   }
 }
 
 class Game extends React.Component {
+  state = {
+    gameStatus: 'new', // new, playing, won, lost
+    remainingSeconds: this.props.initialSeconds,
+    selectedIds: [],
+  };
+  challengeNumbers = Array.from({
+    length: this.props.challengeSize,
+  }).map(() =>
+    randomNumberBetween(...this.props.challengeRange)
+  );
+  target = _.sum(
+    _.sampleSize(this.challengeNumbers, this.props.answerSize)
+  );
 
-  challengeNumbers = Array
-    .from({ length: this.props.challengeSize })
-    .map(() => randomNumberBetween(...this.props.challengeRange));
-
-  target = _.sampleSize(
-    this.challengeNumbers,
-    this.props.challengeSize - 2
-  ).reduce((acc, curr) => acc + curr, 0);
-
+  isNumberAvailable = numberIndex =>
+    this.state.selectedIds.indexOf(numberIndex) === -1;
 
   render() {
+    const { gameStatus, remainingSeconds } = this.state;
     return (
       <div className="game">
         <div className="help">
-          Pick 4 numbers that sum to the target in 15 seconds
+          Pick {this.props.answerSize} numbers that sum to the
+          target in {this.props.initialSeconds} seconds
         </div>
-        <div className="target">{this.target}</div>
+        <div
+          className="target"
+          style={{ backgroundColor: colors[gameStatus] }}
+        >
+          {gameStatus === 'new' ? 'TARGET' : this.target}
+        </div>
         <div className="challenge-numbers">
           {this.challengeNumbers.map((value, index) =>
-           <Number key={index} value={value} />
+            <Number
+              key={index}
+              id={index}
+              value={gameStatus === 'new' ? '?' : value}
+              clickable={this.isNumberAvailable(index)}
+            />
           )}
         </div>
         <div className="footer">
-          <div className="timer-value">15</div>
-          <button>Start</button>
+          {gameStatus === 'new' &&
+            <button>Start Game</button>}
+
+          {gameStatus === 'playing' &&
+            <div className="timer-value">{remainingSeconds}</div>
+          }
+
+          {['won', 'lost'].includes(gameStatus) &&
+            <button>Play Again</button>
+          }
         </div>
       </div>
     );
   }
 }
 
-export default Game;
+class Page extends React.Component {
+
+  render() {
+    return (
+      <Game
+        challengeRange={[2, 9]}
+        challengeSize={6}
+        answerSize={4}
+        initialSeconds={15}
+      />
+    )
+  }
+}
+
+export default Page;
