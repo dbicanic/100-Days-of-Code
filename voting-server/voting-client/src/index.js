b66e8547d8d59475e33f266b9ca710fa19f6c2af
dbicanic <dbicanic77@gmail.com>
2018-01-16 21:17:37 -0600

    Ready to try jest testing for voting

--- voting-server/voting-client/src/index.js
+++ voting-server/voting-client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+	<Voting pair={pair} />,
+	document.getElementById('root')
+);
 registerServiceWorker();
