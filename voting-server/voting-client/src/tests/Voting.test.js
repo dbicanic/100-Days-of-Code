b9c5e3967ccd56c767dc15ed2a864743f0c54905
dbicanic <dbicanic77@gmail.com>
2018-01-18 22:44:25 -0600

    Trying to figure out all of jest

--- voting-server/voting-client/src/tests/Voting.test.js
+++ voting-server/voting-client/src/tests/Voting.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Voting from '../../src/components/Voting';
+import Voting from '../components/Voting';
 import renderer from 'react-test-renderer';
 import { shallow, mount, render } from 'enzyme';
 
 describe('voting', () => {
 	test('it renders a pair of buttons', () => {
-		const component = renderer.create(
+		const component = shallow(
 			<Voting pair={["Trainspotting", "28 Days Later"]} />
 		);
-		expect(component.find('button')).to.have.length(2);
+		console.log(component.find('button'));
+		// expect(comp);
 	});
 });
 
