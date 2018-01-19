0e498b1f40898cbc45930415382ff4d848e3714e
dbicanic <dbicanic77@gmail.com>
2018-01-18 23:08:34 -0600

    Update test, need to figure on how to ad enyzme again

---
+++ voting-server/voting-client/src/test/Voting.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Voting from '../components/Voting';
+import { shallow, mount, render } from 'enzyme';
+
+describe('voting', () => {
+	test('it renders a pair of buttons', () => {
+		const component = shallow(
+			<Voting pair={["Trainspotting", "28 Days Later"]} />
+		);
+		console.log(component.find('button'));
+		// expect(comp);
+
+	});
+});
