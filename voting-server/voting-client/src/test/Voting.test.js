833180e049c45818b238b7e97997fd7b50c60a05
dbicanic <dbicanic77@gmail.com>
2018-01-19 22:02:04 -0600

    Get jest working

--- voting-server/voting-client/src/test/Voting.test.js
+++ voting-server/voting-client/src/test/Voting.test.js
@@ -7,8 +7,22 @@ describe('voting', () => {
 		const component = shallow(
 			<Voting pair={["Trainspotting", "28 Days Later"]} />
 		);
-		console.log(component.find('button'));
-		// expect(comp);
 
+		expect(component.find('button').length).toBe(2);
+		expect(component.find('button').first().text()).toBe("Trainspotting");
+		expect(component.find('button').last().text()).toBe("28 Days Later");
+
+	});
+
+	test('it invokes a callback when a button is clicked', () => {
+		let voteWith;
+		const vote = (entry) => voteWith = entry;
+
+		const component = shallow(
+			<Voting pair={["Trainspotting", "28 Days Later"]} vote={vote} />
+		);
+
+		const buttons = component.find('button');
+		buttons.first().simulate('click');
 	});
 });
